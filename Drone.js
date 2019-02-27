afa46d8b63c4337f7dd7074c4a18ad66fcf316bc
Alfonso <alfonsolasaguirre@gmail.com>
2019-02-27 22:05:22 +0100

    sending stream

--- Drone.js
+++ Drone.js
@@ -1,57 +1,82 @@
-
-const dgram = require('dgram')
+const dgram = require("dgram");
 
 //drone information
-const { droneInfo } = require('./droneConfig')
+const { droneInfo } = require("./droneConfig");
 
 //tello sdk commands
-const commands = require('./telloSdk')
+const commands = require("./telloSdk");
 
 class Drone {
-    constructor(customConfig = {}, SDKCommands = commands){
-        this.config = {
-            ...droneInfo,
-            ...customConfig
-        }
+  constructor({customConfig = {}, SDKCommands = commands, socket}) {
+    this.config = {
+      ...droneInfo,
+      ...customConfig
+    };
 
-        //Drone socket
-        this.drone = dgram.createSocket('udp4')
-        this.drone.bind(this.config.PORT)
-        
-        //this.drone.send('command', this.config.PORT, this.config.IP, this._handleError)
-        //this.drone.send('battery?', this.config.PORT, this.config.IP, this._handleError)
+    this.socket = socket;
 
-        //drone listening 0.0.0.0:PORT
+    // Drone socket
+    this.drone = dgram.createSocket("udp4");
+    this.drone.bind(this.config.PORT);
 
-       this.drone.on('error', err => {
-            console.log(`drone error:\n${err.stack}`);
-            this.drone.close();
-        })
-        
-        this.drone.on('message', (msg, rinfo) => {
-            console.log(`drone got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-          });
-          
-        this.drone.on('listening', () => {
-            const address = this.drone.address();
-            console.log(`drone listening ${address.address}:${address.port}`);
-        });        
+    // Drone state socket
+    this.droneState = dgram.createSocket("udp4");
+    this.droneState.bind(this.config.LISTEN_STATE_PORT);
+
+    this.droneStream = dgram.createSocket("udp4");
+    this.droneStream.bind(this.config.LISTEN_VIDEO_PORT);
+
+    // Drone stream socket
+    this.drone.send(
+      "command",
+      this.config.PORT,
+      this.config.IP,
+      this._handleError
+    );
+    this.drone.send(
+      "streamon",
+      this.config.PORT,
+      this.config.IP,
+      this._handleError
+    );
+    //drone listening 0.0.0.0:PORT
+
+    this.drone.on("message", (msg, rinfo) => {
+      console.log(`drone got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    });
+
+    // Drone state callback
+    this.droneState.on("message", (msg, rinfo) => {
+      console.log(
+        // `drone state got: ${msg} from ${rinfo.address}:${rinfo.port}`
+      );
+    });
+
+    this.droneStream.on("message", (msg, rinfo) => {
+        console.log('enviado stream')
+        this.socket.emit('stream', msg)
+      //console.log(`drone stream got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    });
+  }
+
+  // PRIVATE METHODS
+  _handleError(error) {
+    if (error) {
+      console.log(error);
+      this.drone.close();
     }
+  }
 
-    // PRIVATE METHODS
-    _handleError(error) {
-        if(error){
-            console.log(error)
-            this.drone.close()
-        }
-    }
-
-    // Send command method
-    command(cmd) {
-        this.drone.send('command', this.config.PORT, this.config.IP, this._handleError)
-        this.drone.send(cmd, this.config.PORT, this.config.IP, this._handleError)
-    }
-
+  // Send command method
+  command(cmd) {
+    this.drone.send(
+      "command",
+      this.config.PORT,
+      this.config.IP,
+      this._handleError
+    );
+    this.drone.send(cmd, this.config.PORT, this.config.IP, this._handleError);
+  }
 }
 
-module.exports = Drone
+module.exports = Drone;
--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 //Load server
-const app = require('express')()
-const http = require('http').Server(app)
-const socket = require('socket.io')(http)
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const app = require("express")();
+const http = require("http").Server(app);
+const socket = require("socket.io")(http);
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const commands = ['command', 'battery?', 'takeoff', 'land'];
-const { droneInfo } = require('./droneConfig')
+const commands = ["command", "battery?", "takeoff", "land"];
+const { droneInfo } = require("./droneConfig");
 
 //Get server port
-const PORT = process.env.PORT || 3020
+const PORT = process.env.PORT || 3020;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors()); //https://daveceddia.com/access-control-allow-origin-cors-errors-in-react-express
 
 //Drone
-const Drone = require('./Drone')
-const drone = new Drone()
-drone.command('time?')
-drone.command('battery?')
+const Drone = require("./Drone");
+const drone = new Drone({
+  socket: socket
+});
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 
-app.get('/command', (req, res) => {
-    drone.send('comando', 4, droneInfo.PORT, droneInfo.IP, (err) => console.log(err))
-    res.send('<h1>Comando</h1>')
-})
+app.get("/command", (req, res) => {
+  drone.send("comando", 4, droneInfo.PORT, droneInfo.IP, err =>
+    console.log(err)
+  );
+  res.send("<h1>Comando</h1>");
+});
 
-app.post('/', (req, res) => {
-    const { cmd } = req.body
-    console.log('comando recibido ', cmd)
-    drone.command(cmd)
-    res.json({
-        status: 320
-    })
-})
+app.post("/", (req, res) => {
+  const { cmd } = req.body;
+  console.log("comando recibido ", cmd);
+  drone.command(cmd);
+  res.json({
+    status: 320
+  });
+});
 
 //sockect connecion
-socket.on('connection', function(socket){
-    console.log('Conectado al socket');
-    socket.on('command', command => {
-        try {
-            console.log(`recived command ${command}`)
-            drone.command(command)
-        }catch {
-            console.log('El dron no esta conectado')
-        }
-    })
-    socket.on('disconnect', () => {
-        console.log('Desconectado del socket');
-      });
+socket.on("connection", function(socket) {
+  console.log("Conectado al socket");
+  socket.on("command", command => {
+    try {
+      console.log(`recived command ${command}`);
+      drone.command(command);
+    } catch(err) {
+      console.log("El dron no esta conectado");
+    }
   });
+  socket.on("disconnect", () => {
+    console.log("Desconectado del socket");
+  });
+});
 
-http.listen(PORT, () => console.log(`Server running at port http://localhost:${PORT}`))
+http.listen(PORT, () =>
+  console.log(`Server running at port http://localhost:${PORT}`)
+);
